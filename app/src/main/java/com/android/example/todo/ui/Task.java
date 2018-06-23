cfbead1fd0b0b792be3778fb8628ec8221df5c0a
Gianpiero Mode <gianmode1803@hotmail.com>
2018-06-23 11:13:55 -0400

    Se crea, se elimina y se busca en la base de datos, falta el actualizar y el detalle con el eliminar... falta tambien la documentación

--- app/src/main/java/com/android/example/todo/ui/Task.java
+++ app/src/main/java/com/android/example/todo/ui/Task.java
@@ -1,8 +1,11 @@
 package com.android.example.todo.ui;
 
 import java.util.Date;
+import java.util.List;
 
+import io.realm.Realm;
 import io.realm.RealmObject;
+import io.realm.RealmResults;
 import io.realm.annotations.PrimaryKey;
 
 public class Task extends RealmObject {
@@ -11,6 +14,7 @@ public class Task extends RealmObject {
     private long id;
 
     private String task;
+    //EL status será "PENDIENTE" o "COMPLETADO"
     private String status;
     private Date dateOfCreation;
 
@@ -58,5 +62,89 @@ public class Task extends RealmObject {
     public void setDateOfCreation(Date dateOfCreation) {
         this.dateOfCreation = dateOfCreation;
     }
-    
+
+
+    public boolean createNewTask(Task queryTask){
+        try{
+            // Get a Realm instance for this thread
+            Realm realm = Realm.getDefaultInstance();
+
+            realm.beginTransaction();
+            long nextID = nextId();
+            if(nextID == 0){
+                return false;
+            }
+            queryTask.setId(nextID);
+            Task realmTask = realm.copyToRealmOrUpdate(queryTask);
+            realm.commitTransaction();
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    public long nextId(){
+        try{
+            // Get a Realm instance for this thread
+            Realm realm = Realm.getDefaultInstance();
+            Number num = realm.where(Task.class).max("id");
+            int nextID;
+            if(num == null) {
+                nextID = 1;
+            } else {
+                nextID = num.intValue() + 1;
+            }
+            return nextID;
+        }catch (Exception e){
+            return 0;
+        }
+    }
+
+    public List<Task> getAllTasks(){
+        try{
+            // Get a Realm instance for this thread
+            Realm realm = Realm.getDefaultInstance();
+            RealmResults<Task> queryTask = realm.where(Task.class).findAll();
+            if (queryTask.isEmpty())
+                return null;
+            else
+                return queryTask;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    //Falta ordenarlos
+    public List<Task> getSpecificTasks(String queryStatus){
+        try{
+            // Get a Realm instance for this thread
+            Realm realm = Realm.getDefaultInstance();
+            RealmResults<Task> queryTask = realm.where(Task.class).equalTo("status", queryStatus).findAll();
+            if (queryTask.isEmpty())
+                return null;
+            else
+                return queryTask;
+        }catch (Exception e){
+            return null;
+        }
+    }
+
+    //Pendiente, si hay dos que se llaman igual???
+    public boolean deleteTask(String queryTask){
+        try{
+            // Get a Realm instance for this thread
+            Realm realm = Realm.getDefaultInstance();
+            RealmResults<Task> queryTaskforDelete = realm.where(Task.class).equalTo("task", queryTask).findAll();
+            if (!queryTaskforDelete.isEmpty()){
+                queryTaskforDelete.get(0).deleteFromRealm();
+                return true;
+            }else{
+              return false;
+            }
+        }catch (Exception e){
+            return false;
+        }
+    }
+
+    //FALTA EL UPDATE
 }
