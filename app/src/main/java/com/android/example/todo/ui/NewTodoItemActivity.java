f225c858639f889948379f280b282888f92c6819
unknown <jonathanaveledo@gmail.com>
2018-06-26 15:54:20 -0400

    Agregada la funcionalidad de agregar una nueva tarea llamando a la actividad task

--- app/src/main/java/com/android/example/todo/ui/NewTodoItemActivity.java
+++ app/src/main/java/com/android/example/todo/ui/NewTodoItemActivity.java
@@ -6,21 +6,29 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TextView;
 
 import com.android.example.todo.R;
 
-public class NewTodoItemActivity extends AppCompatActivity {
+import java.util.Date;
 
+public class NewTodoItemActivity extends AppCompatActivity {
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_todo_item);
 
         Button button = (Button)findViewById(R.id.button);
+        textView = (TextView)findViewById(R.id.editText);
+        final CalendarView calendarView = (CalendarView) findViewById(R.id.calendarView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Task newTask = new Task(0,textView.getText() + "","PENDIENTE", new java.util.Date(),
+                        new Date(calendarView.getDate()), null);
+                newTask.createAnUpdateNewTask(newTask);
             }
         });
 
